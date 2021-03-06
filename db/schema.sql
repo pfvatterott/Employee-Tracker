2f55da880bc0804a280e344bad368e88e190548c
Paul <pfvatterott@gmail.com>
2021-03-06 08:56:54 -0700

    Created seed csv files

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,26 @@ CREATE database Employee_tracker;
 USE Employee_tracker;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(100) NOT NULL,
+  id INT(10) NOT NULL,
+  deptname VARCHAR(100) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-	id INT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY (id)
+	id INT(10) NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT(10) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-	id INT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY (id)
+	id INT(10) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT(10) NOT NULL,
+  manager_id INT(10),
+  PRIMARY KEY (id)
 );
 
 SELECT * FROM department;
